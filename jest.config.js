1dc6665b2aeb62605e154a9398bf0fb1341e44cc
Sergey Bogdanov <sergey.bogdanov@gmail.com>
2020-09-15 14:55:40 +0300

    feat: интегрирован emailService, который отсылвает сообщения через AWS SES и используя handlebars как email template. Добавлены тесты и один интеграционный, который запускается если в jest.env.dev.js содержит необходимые env для отсылки. testController.spec.js переделан под .ts, чтобы придерживаться одного стиля.

---
+++ jest.config.js
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) Mesto.co
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const fs = require('fs');
+
+module.exports = {
+  roots: ['<rootDir>'],
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest'
+  },
+  setupFiles: [returnIfExists('./jest.env.dev.js')].filter(t => t !== null),
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
+
+function returnIfExists(filePath) {
+  if (fs.existsSync(filePath))
+    return filePath;
+  else
+    return null;
+}
---
+++ test/emailService.spec.ts
@@ -0,0 +1,33 @@
+/**
+ * Copyright (c) Mesto.co
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import {getCompiledTemplate, sendEmailByTemplate} from '../src/emailService';
+import * as fs from 'fs';
+
+test('should compile templates', async () => {
+  const compiledTemplate = getCompiledTemplate('test');
+  expect(compiledTemplate).toBeDefined();
+  const result = compiledTemplate({'name': 'ABC', 'link': 'http://magicLink'});
+  expect(result).toEqual(fs.readFileSync('./test/data/expectedOutput.html', 'utf8'));
+});
+
+// Integration test for AWS SES
+if (!!process.env.AWS_ACCESS_KEY_ID && !process.env.CI) {
+  test('should send email by template based on passed variables', async () => {
+    await sendEmailByTemplate('test', 'sergey@songtive.com', 'тест тема', {
+      'name': 'ABC', 'link': 'http://magicLink'
+    });
+  });
+}
