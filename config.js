072504b7da70b0c99a8149561261bf91f1abf6b1
Aleksei Koziatinskii <ak239spb@gmail.com>
2020-10-18 20:12:36 -0700

    feat: методы для работы с openland
    Все методы требует авторизации.
    
    POST /v1/openland/sendCode
    Принимает один параметр:
    - openland - строка, максимальная длина 64 символов, может быть
      как id пользователя, так и его shortname, так и ссылка,
      включающая в себя id, либо shortname, так и @shortname -
      в тесте есть примеры.
    Возвращает: {
      codeId: string
    }
    Время жизни кода - 2 минуты.
    
    POST /v1/openland/verifyCode
    Принимает два параметра:
    - codeId - значение полученное из предыдущего метода,
    - code - строка в 6 символов с кодом верификации.
    Методо возвращает 200 только, если все в порядке, во всех остальных
    случаях - 401: код истек, неверный пользователь, неверный код.
    
    POST /v1/openland/getUser
    Принимает один параметр:
    - openland - строка, максимальная длина 64 символов, может быть
      как id пользователя, так и его shortname, так и ссылка,
      включающая в себя id, либо shortname, так и @shortname -
      в тесте есть примеры.
    Возвращает: {
      fullName: string,
      imagePath: string|undefined,
      shortname: string|undefined,
      location: string|undefined,
      about: string|undefined,
      contacts: {title: string, url: string}[]
      skills: string[]|undefined
    }
    Первый контакт - всегда контакт с title openland.

--- config.js
+++ config.js
@@ -58,7 +58,10 @@ const lambdaConfig = {
   googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY,
   corsOrigin: process.env.CORS_ORIGIN || 'https://app.mesto.co/',
   adminUserId: process.env.ADMIN_USER_ID,
-  enableMethodsForTest: false
+  enableMethodsForTest: false,
+  openlandToken: process.env.OPENLAND_TOKEN,
+  // node -e "console.log(crypto.randomBytes(32).toString('hex'))"
+  openlandCodeSecret: process.env.OPENLAND_CODE_SECRET
 };
 
 const developmentConfig = {
@@ -103,7 +106,9 @@ const developmentConfig = {
 
   corsOrigin: '*',
   adminUserId: '00000000-1111-2222-3333-000000000009',
-  enableMethodsForTest: true
+  enableMethodsForTest: true,
+  openlandToken: process.env.OPENLAND_TOKEN || 'YYeXUMHV_40G3jKvYV2Bz1wIfxpWUB9hfWbXmVkEormuC3jTT2ZqbUWOMw4LGtKxVhqmzA-7ds_Bp3MSXEeW5A',
+  openlandCodeSecret: process.env.OPENLAND_CODE_SECRET || '4003fe4c92ed4ac97363c7528e30bbb798c59fefbcfb35ffe6ef937c45d86d86'
 };
 
 const config = {
