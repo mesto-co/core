cffe4fd3d5ea6306b118aef6044c87488dfdc4c8
Aleksei Koziatinskii <alekseik@netflix.com>
2021-05-24 13:59:56 -0700

    feat: added v1/admin/removeOldTokens
    Метод удаляет все просроченные токены для входа. Будем вызывать его время
    от времени, чтобы токены не накапливались.

--- migrations/20210524134140_permission_removeoldtokens.js
+++ migrations/20210524134140_permission_removeoldtokens.js
@@ -13,16 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-enum Permission {
-    ADDDELPERMISSION = 1,
-    ACTIVATEUSER = 2,
-    BANUSER = 3,
-    ADDUSER = 4,
-    SENDINVITEEMAIL = 5,
-    NEWUSERS = 6,
-    EVENT = 7,
-    DELEVENT = 8,
-    REMOVEOLDTOKENS = 9
-}
 
-export { Permission };
+exports.up = function(knex) {
+  return knex.raw(`INSERT INTO permission(id, description) VALUES (9, 'Remove old tokens')`);
+};
+
+exports.down = function(knex) {
+  return knex.raw(`DELETE FROM permission WHERE id = 9`);
+};
