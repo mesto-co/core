4ed9a3a279b12a09471307e37e7b48ec7b61f23a
Aleksei Koziatinskii <ak239spb@gmail.com>
2022-01-28 12:34:02 -0800

    feat: add a permission that allows to update any user profile
    Request with a token that includes UPDATEUSER can update any
    user profile with userId passed as request param.

--- migrations/20220128122256_add_permission.js
+++ migrations/20220128122256_add_permission.js
@@ -13,18 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-enum Permission {
-    ADDDELPERMISSION = 1,
-    ACTIVATEUSER = 2,
-    BANUSER = 3,
-    ADDUSER = 4,
-    SENDINVITEEMAIL = 5,
-    NEWUSERS = 6,
-    EVENT = 7,
-    DELEVENT = 8,
-    REMOVEOLDTOKENS = 9,
-    RESOLVEEMAIL = 14,
-    UPDATEUSER = 15,
-}
 
-export { Permission };
+exports.up = async function(knex) {
+  await knex.raw(`INSERT INTO permission(id, description) VALUES (15, 'Update user by id')`);
+};
+
+exports.down = async function(knex) {
+  await knex.raw(`DELETE FROM permission WHERE id = 15`);
+};
