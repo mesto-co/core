f8bcbde6bdf549c7c7dd1cbf2d5c84be4a4fdb1a
Aleksei Koziatinskii <ak239spb@gmail.com>
2022-02-20 01:44:27 -0800

    feat: process amoCRM updates
    If there is a contact update - try to update email first and add
    a new awaiting contact just in case if it is a new user.
    
    If contact is removed or got a Забанен tag - set status to closed.
    
    If amo_lead_id is updated and set to status 142 - set status to
    approved.

--- src/controllers/amoController.ts
+++ src/controllers/amoController.ts
@@ -14,7 +14,9 @@
  * limitations under the License.
  */
 import express from 'express';
+import knex from '../knex';
 import {getArgs} from '../utils';
+import {UserStatus} from '../enums/UserStatus';
 
 const {
   amoToken
@@ -33,7 +35,77 @@ amoController.route('/')
     .post(async (request, response) => {
       const args = getArgs(request);
       if (args && args.token === amoToken) {
-        console.debug('amoController#post', args);
+        const emailUpdates = [];
+        const leadsUpdates = [];
+        const contactsDeleted = [];
+
+        for (let i = 0; true; ++i) {
+          const prefix = `contacts[update][${i}]`;
+          const id = args[prefix + '[id]'];
+          if (id) {
+            for (let j = 0; true; ++j) {
+              if (args[prefix + `[custom_fields][${j}][code]`] === 'EMAIL') {
+                emailUpdates.push({
+                  email: args[prefix + `[custom_fields][${j}][values][0][value]`].toLowerCase(),
+                  id: id,
+                  name: args[prefix + '[name]']
+                });
+                break;
+              } else if (!args[`contacts[update][${i}][custom_fields][${j}][id]`]) {
+                break;
+              }
+            }
+            for (let j = 0; true; ++j) {
+              const name = args[prefix + `[tags][${j}][name]`];
+              if (!name)
+                break;
+              if (args[prefix + `[tags][${j}][name]`] === 'Забанен')
+                contactsDeleted.push(id);
+            }
+            for (const arg in args) {
+              if (arg.startsWith(prefix + '[linked_leads_id]')) {
+                leadsUpdates.push({
+                  id: id,
+                  leadId: args[arg];
+                });
+                break;
+              }
+            }
+          } else {
+            break;
+          }
+        }
+
+        for (let i = 0; true; ++i) {
+          const id = args[`contacts[delete][${i}][id]`];
+          if (id)
+            contactsDeleted.push(id);
+          else
+            break;
+        }
+
+        let approvedLeads = [];
+        for (let i = 0; true; ++i) {
+          const id = args[`leads[update][${i}][id]`];
+          if (id) {
+            if (args[`leads[update][${i}][status_id]`] === '142')
+              approvedLeads.push(id);
+          } else {
+            break;
+          }
+        }
+
+        for (const emailUpdate of emailUpdates)
+          await knex.raw('UPDATE "User" SET email = ? WHERE amo_id = ?', [emailUpdate.email, emailUpdate.id]);
+        for (const emailUpdate of emailUpdates)
+          await knex.raw('INSERT INTO "User" (email, "fullName", amo_id, status, about) VALUES (?, ?, ?, ?, ?) ON CONFLICT DO NOTHING', [emailUpdate.email, emailUpdate.name, emailUpdate.id, 'awaiting', '']);
+        for (const leadUpdate of leadsUpdates)
+          await knex.raw('UPDATE "User" SET amo_lead_id = ? WHERE amo_id = ?', [leadUpdate.leadId, leadUpdate.id]);
+        for (const id of contactsDeleted)
+          await knex.raw('UPDATE "User" SET status = ? WHERE amo_id = ?', [UserStatus.CLOSED, id]);
+        for (const leadId of approvedLeads)
+          await knex.raw('UPDATE "User" SET status = ? WHERE amo_lead_id = ?', [UserStatus.APPROVED, leadId]);
+
         return response.status(200).json({}).end();
       }
       return response.status(401).json({}).end();
